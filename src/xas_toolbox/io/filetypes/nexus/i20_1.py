1f22581581281983f0e2570ab69c4af8a94cf815
Haddad, Lucy (DLSLtd,RAL,CEO) <l.haddad@diamond.ac.uk>
2026-09-18 12:32:32 +0000

    stash most recent changes (leaving branch for a while)

--- src/xas_toolbox/io/filetypes/nexus/i20_1.py
+++ src/xas_toolbox/io/filetypes/nexus/i20_1.py
@@ -1,27 +1,35 @@
 """
 Reader for legacy beamline i20-1 (EDE).
 """
+
 import logging
 import re
-from typing import Union
+
 import h5py
+
 from xas_toolbox.utils.scan_data import ElementMeta
+
 from .nxs_reader import NxsReader
 
 logger = logging.getLogger(__name__)
 
-class I20_1Reader(NxsReader):
+
+class I20_1Reader(NxsReader):  # noqa: N801
     """
     Class to hold relevant paths to XAS data for beamline i20-1 (EDE).
 
     Attributes:
-        transParams (dict): Paths to transmission data: <b>mutrans, energy, i0, itrans</b>.
+        transParams (dict): Paths to transmission data: <b>mutrans, energy, i0, itrans
+        </b>.
         fluorParams (dict): Paths to fluorescence data: <b>mufluor, energy, ifluor</b>.
-        referParams (dict): Paths to reference data: <b>murefer, irefer</b>, these will all be
+        referParams (dict): Paths to reference data: <b>murefer, irefer</b>, these will
+          all be
                             `None`.
-        mcaParams (dict): Paths to MCA data <b>dtc_factors, raw_scaler_in_window, mcas</b>,
+        mcaParams (dict): Paths to MCA data <b>dtc_factors, raw_scaler_in_window, mcas<
+        /b>,
                             these will all be `None`.
     """
+
     def __init__(self, path):
         super().__init__(path)
 
@@ -30,17 +38,21 @@ class I20_1Reader(NxsReader):
 
         # i20-1 specific paths:
         self.scanMeta["repetition_files"] = None
-        self.elementMeta["_path"] = ["entry1/metaData", "polynomial", "referenceDataFileName"]
+        self.elementMeta["_path"] = [
+            "entry1/metaData",
+            "polynomial",
+            "referenceDataFileName",
+        ]
 
         self.monoParams["energy"] = "entry1/lnI0It/energy"
         self.monoParams["i0"] = "entry1/metaData/i0"
-        
+
         self.transParams["mutrans"] = "entry1/lnI0It/data"
-        self.transParams["itrans"] = "entry1/metaData/it" 
+        self.transParams["itrans"] = "entry1/metaData/it"
 
         self._make_fluor_paths()
 
-    def _make_fluor_paths(self)->None:
+    def _make_fluor_paths(self) -> None:
         """
         If xh/frelon used in measurement, populate the fluorParams
         dict with the correct nexus file paths to data.
@@ -53,7 +65,7 @@ class I20_1Reader(NxsReader):
         else:
             logger.info("Alt. detector cannot be found.")
 
-    def _make_elementMeta(self)->None:
+    def _make_elementMeta(self) -> None:  # noqa: N802
 
         mdetails = self.elementMeta["_path"]
         with h5py.File(self.path, "r") as f:
@@ -61,11 +73,13 @@ class I20_1Reader(NxsReader):
                 meta = f[mdetails[0]]
                 if mdetails[1] in meta.attrs.keys():
                     info = meta.attrs[f"{mdetails[1]}"][:].decode("utf-8")
-                else: return
+                else:
+                    return
 
-                sample = list(filter(lambda p: (mdetails[-1] in p), info.split(",")))
-                if len(sample) < 1: return
-                sdetails = re.findall("[A-Z][a-z]?_[A-Z]\d?",sample[0])[0]
+                sample = list(filter(lambda p: mdetails[-1] in p, info.split(",")))
+                if len(sample) < 1:
+                    return
+                sdetails = re.findall("[A-Z][a-z]?_[A-Z]\\d?", sample[0])[0]
                 symbol, edge = sdetails.split("_")
                 self.elementMeta["symbol"] = symbol
                 self.elementMeta["edge"] = edge
@@ -74,16 +88,16 @@ class I20_1Reader(NxsReader):
 
             else:
                 return
-            
-    def _get_ElementMeta(self, val:ElementMeta)->Union[str, None]:
+
+    def _get_ElementMeta(self, val: ElementMeta) -> str | None:  # noqa: N802
         """
         Get element metadata from scan (e.g. absorbing edge and element).
         """
         if val not in self.elementMeta.keys():
-            logger.warning(f"{val} not present in file"); return
-        
+            logger.warning(f"{val} not present in file")
+            return
+
         if self.elementMeta[val] is None:
             self._make_elementMeta()
 
         return self.elementMeta[val]
-            
--- src/xas_toolbox/io/filetypes/nexus/nxs_reader.py
+++ src/xas_toolbox/io/filetypes/nexus/nxs_reader.py
@@ -4,13 +4,15 @@ Base reader for nexus files.
 
 import logging
 from pathlib import Path
-from typing import Union
-from xas_toolbox.utils.scan_data import ScanData, ScanMeta, ElementMeta
+
 import h5py
-import numpy as np
 import lxml.etree as etree
+import numpy as np
+
+from xas_toolbox.utils.scan_data import ElementMeta, ScanData, ScanMeta
 
 logger = logging.getLogger(__name__)
+
 
 class NxsReader:
     """
@@ -22,20 +24,25 @@ class NxsReader:
 
     Attrs:
         path (Path): Path to nexus file.
-        scanMeta (dict): Dictionary of paths to <i>start_time, end_time</i> and 
+        scanMeta (dict): Dictionary of paths to <i>start_time, end_time</i> and
                         <i>repetition_files</i>.
-        elementMeta (dict): Empty dictionary to be given paths to <i>symbol, edge, reference</i>
+        elementMeta (dict): Empty dictionary to be given paths to <i>symbol, edge,
+          reference
+        </i>
                             and <i>ref_edge</i>.
-        transParams (dict): Empty dictionary to be given paths to <i>mutrans, energy, i0</i> and
+        transParams (dict): Empty dictionary to be given paths to <i>mutrans, energy, i0
+        </i> and
                             <i>itrans</i>.
-        fluorParams (dict): Empty dictionary to be given paths to <i>mufluor, energy</i> and 
+        fluorParams (dict): Empty dictionary to be given paths to <i>mufluor, energy</i
+        > and
                             <i>ifluor</i>.
-        referParams (dict): Empty dictionary to be given paths to <i>murefer</i> and 
+        referParams (dict): Empty dictionary to be given paths to <i>murefer</i> and
                             <i>irefer</i>.
-        mcaParams (dict): Empty dictionary to be given paths to <i>dtc_factors, 
+        mcaParams (dict): Empty dictionary to be given paths to <i>dtc_factors,
                           raw_scaler_in_window</i> and <i>mcas</i>.
     """
-    def __init__(self, path:Path):
+
+    def __init__(self, path: Path):
 
         self.path = path
 
@@ -43,46 +50,31 @@ class NxsReader:
             "start_time": "entry1/start_time",
             "end_time": "entry1/end_time",
             "repetition_files": "entry1/before_scan/files_in_repetition_scan",
-            "path": self.path
+            "path": self.path,
         }
 
-        self.monoParams = {
-            "i0": None,
-            "energy": None
-            }
+        self.monoParams = {"i0": None, "energy": None}
 
         self.elementMeta = {
             # could have functions instead of labels being passed through here?
             "symbol": None,
             "edge": None,
             "reference": None,
-            "ref_edge": None
+            "ref_edge": None,
         }
 
-        self.transParams = {
-            "mutrans": None,
-            "energy": None,
-            "i0": None,
-            "itrans": None
-            }
+        self.transParams = {"mutrans": None, "energy": None, "i0": None, "itrans": None}
 
-        self.fluorParams = {
-            "mufluor": None,
-            "energy": None,
-            "ifluor": None
-            }
+        self.fluorParams = {"mufluor": None, "energy": None, "ifluor": None}
 
-        self.referParams = {
-            "murefer": None,
-            "irefer": None
-            }
+        self.referParams = {"murefer": None, "irefer": None}
 
         self.mcaParams = {
             "dtc_factors": None,
             "raw_scaler_in_window": None,
-            "mcas": None
-            }
-        
+            "mcas": None,
+        }
+
     def _find_detector(self) -> str | None:
         """
         Find fluorescence detector used in a scan if multiple configurations
@@ -94,10 +86,11 @@ class NxsReader:
                 for d in self._detectors:
                     d_tmp = f"entry1/{d}"
                     if d_tmp in f.keys():
-                        det = d; break
+                        det = d
+                        break
         return det
-        
-    def _get_ScanData(self, val:ScanData)->Union[np.ndarray, None]:
+
+    def _get_ScanData(self, val: ScanData) -> np.ndarray | None:  # noqa: N802
         """
         Get a value from the file if it matches any of the values in `ScanData`.<br>
         This is to be used for numeric data in the file (i.e. not metadata).
@@ -106,13 +99,14 @@ class NxsReader:
             val (ScanData): Value from the nexus file to return.
 
         Returns:
-            out (np.ndarray | None): Value as an array. If the item isn't in the file 
+            out (np.ndarray | None): Value as an array. If the item isn't in the file
                                     `None` will be returned.
         """
         out = None
         _val = getattr(ScanData, val)
         tosearch = _val.mode
-        if tosearch is None: tosearch = "monoParams"
+        if tosearch is None:
+            tosearch = "monoParams"
         if tosearch:
             d_tosearch = self.__getattribute__(tosearch)
             if (val in d_tosearch.keys()) and (d_tosearch[val] is not None):
@@ -120,10 +114,10 @@ class NxsReader:
                     out = f[d_tosearch[val]][...]
             else:
                 logger.warning(f"{val} is not present in scan.")
-    
+
         return out
-    
-    def _get_ScanMeta(self, val:ScanMeta)->Union[str, list]:
+
+    def _get_ScanMeta(self, val: ScanMeta) -> str | list:  # noqa: N802
         """
         Get a requested value from the file if it's classed as
         scan-metadata (i.e. `start_time`, `end_time` or
@@ -134,10 +128,15 @@ class NxsReader:
         if val not in self.scanMeta.keys():
             logger.warning(f"{val} not found in file")
             return
-        
-        if val == "path": return self.scanMeta["path"]
-        
+
+        if val == "path":
+            return self.scanMeta["path"]
+
         _path = self.scanMeta[val]
+
+        if _path is None:
+            return None
+
         with h5py.File(self.path, "r") as f:
             if _path in f.keys():
                 out = f[_path][...].astype("T")
@@ -145,14 +144,16 @@ class NxsReader:
                 logger.warning(f"{val} not found in file")
         if hasattr(out, "ndim"):
             if out.ndim == 0:
-                out = str(out) #single values are returned as string
-                if out == "": out = None
+                out = str(out)  # single values are returned as string
+                if out == "":
+                    out = None
             else:
-                out = out[0].split("\n") #repetition files need to be split by \n
-                if out == [""]: out = None #repetition files need to be split by \n
+                out = out[0].split("\n")  # repetition files need to be split by \n
+                if out == [""]:
+                    out = None  # repetition files need to be split by \n
         return out
-    
-    def _get_ElementMeta(self, val:ElementMeta)->Union[str, None]:
+
+    def _get_ElementMeta(self, val: ElementMeta) -> str | None:  # noqa: N802
         """
         Get requested ElementMeta item from file. <br>
         This will need to be different for i20-1 files.
@@ -162,9 +163,10 @@ class NxsReader:
         if (val not in self.elementMeta.keys()) or (self.elementMeta[val] is None):
             logger.warning(f"{val} not found in file")
             return
-        
+
         parser = etree.XMLParser(recover=True)
-        _path = self.elementMeta[val][0]; xml_name = self.elementMeta[val][-1]
+        _path = self.elementMeta[val][0]
+        xml_name = self.elementMeta[val][-1]
 
         with h5py.File(self.path, "r") as f:
             if _path not in f.keys():
@@ -174,22 +176,24 @@ class NxsReader:
             out = root.findall(xml_name)[0].text
 
         return out
-    
-    def get_value(self, val:Union[ScanData, ElementMeta, ScanMeta])\
-                        -> Union[np.ndarray, str, list, None]:
-        
+
+    def get_value(
+        self, val: ScanData | ElementMeta | ScanMeta
+    ) -> np.ndarray | str | list | None:
+
         if val in ScanData.__members__.keys():
             return self._get_ScanData(val)
-        
+
         elif val in ElementMeta.__members__.keys():
             return self._get_ElementMeta(val)
 
         elif val in ScanMeta.__members__.keys():
             return self._get_ScanMeta(val)
-        
-        else: return ValueError("Value not a valid XAS parameter.")
 
-    def _get_dims(self, val:ScanData)->tuple[int]:
+        else:
+            return ValueError("Value not a valid XAS parameter.")
+
+    def _get_dims(self, val: ScanData) -> tuple[int]:
         """
         Get the shape of a requested value.
 
@@ -201,7 +205,8 @@ class NxsReader:
         """
         _val = getattr(ScanData, val)
         tosearch = _val.mode
-        if tosearch is None: tosearch = "monoParams"
+        if tosearch is None:
+            tosearch = "monoParams"
         if tosearch:
             dtmp = self.__getattribute__(tosearch)
             if (val in dtmp.keys()) and (dtmp[val] is not None):
