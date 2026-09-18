3c1c6ef978b5cb3737a6d0c11cfc6079ece320ee
Haddad, Lucy (DLSLtd,RAL,CEO) <l.haddad@diamond.ac.uk>
2026-09-18 14:30:57 +0000

    add deglitching

--- src/xas_toolbox/xas/corrections/__init__.py
+++ src/xas_toolbox/xas/corrections/__init__.py
@@ -1,3 +1,8 @@
+"""
+Submodule for data-correction related functions.
+"""
+
+from .deglitch import median_deglitch
 from .pre_edge import pre_edge
 
-__all__ = ["pre_edge"]
+__all__ = ["pre_edge", "median_deglitch"]
---
+++ src/xas_toolbox/xas/corrections/deglitch.py
@@ -0,0 +1,45 @@
+from functools import partial
+
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+
+
+def median_deglitch(y: np.ndarray, window: int | float, threshold: float) -> np.ndarray:
+    """
+    Apply a median filter on y data. Values with values above standard
+    deviation of `data - median filtered * threshold` will be replaced with the
+    median filtered value.
+
+    Arguments:
+        y (np.ndarray): y data to filter.
+        window (int|float): Window size for rolling median filter.
+        threshold (float): Tolerance for replacement with filtered value.
+
+    Returns:
+        yf (np.ndarray): Filtered array.
+    """
+    # it would be nice to have auto window + threshold finding (would require a \
+    # bit of prior knowledge though)
+    if window % 2 == 0:
+        window += 1
+    pad_width = window // 2
+
+    if y.ndim == 1:
+        yf = np.pad(y, pad_width, mode="edge")
+        yf = np.mean(sliding_window_view(yf, window), axis=1)
+        tol = np.std(yf - y)
+        yf = np.where(np.abs(yf - y) > tol * threshold, y, yf)
+    else:
+        yf = np.apply_along_axis(
+            partial(np.pad, pad_width=pad_width, mode="edge"), axis=1, arr=y
+        )
+        yf = np.mean(sliding_window_view(yf, window, axis=1), axis=-1)
+        tol = np.std(yf - y, axis=1)
+
+        # could be changed..?
+        for i in range(y.shape[0]):
+            yf[i, :] = np.where(
+                np.abs(yf[i, :] - y[i, :]) > tol[i] * threshold, y[i, :], yf[i, :]
+            )
+
+    return yf
